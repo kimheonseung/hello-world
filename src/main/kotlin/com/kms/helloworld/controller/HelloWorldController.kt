0a2f023700ea91594bc79147326112339de10821
kimheonseung <khs920210@gmail.com>
2024-09-23 23:11:41 +0900

    spring cloud kubernetes 라이브러리 추가

--- src/main/kotlin/com/kms/helloworld/controller/HelloWorldController.kt
+++ src/main/kotlin/com/kms/helloworld/controller/HelloWorldController.kt
@@ -1,5 +1,6 @@
 package com.kms.helloworld.controller
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -7,8 +8,15 @@ import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/hello-world")
-class HelloWorldController {
+class HelloWorldController(
+    @Value("\${cm.custom.message}")
+    val customMessage: String,
+    @Value("\${sec.custom.message}")
+    val customSecMessage: String,
+) {
+
+
 
     @GetMapping
-    fun getHelloWorld() = "hello! ${LocalDateTime.now()}"
+    fun getHelloWorld() = "hello! ${LocalDateTime.now()}. cmCustomMessage: $customMessage, customSecMessage: $customSecMessage"
 }
